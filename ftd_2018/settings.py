13e743c6e611cdd268f6d3099459faee300e5b32
naity <yuan.allen.tian@gmail.com>
2017-11-29 23:10:51 -0800

    Added and updated files for heroku

--- ftd_2018/settings.py
+++ ftd_2018/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # define MEDIA_ROOT and MEDIA_URL for uploading files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
@@ -141,3 +146,7 @@ try:
 except ImportError:
     pass
 
+# database
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
